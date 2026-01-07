4b6a1928bd0de9f1e74d775de789ec6da9adabaf
Guilherme Felipe da Silva Costa <guilherme.costa300@etec.sp.gov.br>
2026-01-07 19:56:55 -0300

    criação do arquivo connection.js e código do mesmo.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
@@ -9,14 +9,14 @@ app.use(express.json());
 const porta = process.env.PORT || 3333;
 
 app.listen(porta, () => {
-    console.log('API rodando na porta 3333')
+  console.log("API rodando na porta 3333");
 });
 
-app.get('/teste', (req, res) => {
-    const agora = new Date().toLocaleString('pt-BR');
-    res.status(200).json({
-        sucesso: true,
-        mensagem: 'Teste de rota GET',
-        data: agora
-    });
-})
+app.get("/teste", (req, res) => {
+  const agora = new Date().toLocaleString("pt-BR");
+  res.status(200).json({
+    sucesso: true,
+    mensagem: "Teste de rota GET",
+    data: agora,
+  });
+});
