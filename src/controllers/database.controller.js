4f7a4fa31ea3ea3da381b6e2a43d01efe0ba232d
Guilherme Felipe da Silva Costa <guicafubr@gmail.com>
2026-03-03 20:17:26 -0300

    Mensagem do commit

--- src/controllers/database.controller.js
+++ src/controllers/database.controller.js
@@ -2,7 +2,8 @@ const pool = require('../config/database');
 
 exports.testDataBase = async (req, res) => {
     try {
-        const [produto] = await pool.query("SELECT * FROM produto WHERE pdt_id = ?", [1]);
+        const [produto] = await pool.query("SELECT * FROM produto WHERE pdt_id = ?", 
+        [1]);
         res.json(produto);
     } catch (error) {
         console.error(error);
