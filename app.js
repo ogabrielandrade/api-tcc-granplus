5567ba16bead26976eb00385fa49bf29daf285cd
Guilherme Felipe da Silva Costa <guilherme.costa300@etec.sp.gov.br>
2026-03-06 22:37:17 -0300

    Add authentication feature with login route and controller

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const cors          = require('cors');
 const productRoutes = require('./src/routes/product.routes');
 const stockRoutes   = require('./src/routes/stock.routes');
 const inputRoutes   = require('./src/routes/input.routes');
-const exitRoutes    = require('./src/routes/exit.routes')
+const exitRoutes    = require('./src/routes/exit.routes');
+const authRoutes    = require('./src/routes/auth.routes')
 
 const app = express();
 
@@ -14,6 +15,8 @@ app.use('/produtos', productRoutes);
 app.use('/estoque', stockRoutes);
 app.use('/', inputRoutes);
 app.use('/', exitRoutes);
+
+app.use("/", authRoutes);
 
 app.use('/database', require('./src/routes/database.routes'));
 
